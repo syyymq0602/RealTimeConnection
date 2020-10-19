38f043582e847b527cdcc928a40272def8e13850
tpl <tpl_vis_group@outlook.com>
2020-10-19 22:29:46 +0800

    Learning

---
+++ GrpcServer/GrpcServer.Web/Services/MyEmployeeService.cs
@@ -0,0 +1,42 @@
+﻿using Grpc.Core;
+using GrpcServer.Web.Data;
+using GrpcServer.Web.Protos;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GrpcServer.Web.Services
+{
+    public class MyEmployeeService :EmployeeService.EmployeeServiceBase
+    {
+        private readonly ILogger<MyEmployeeService> _logger;
+        public MyEmployeeService(ILogger<MyEmployeeService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<EmployeeResponse>
+            GetByNo(GetByNoRequest request,ServerCallContext context)
+        {
+            var md = context.RequestHeaders;
+            foreach(var pair in md)
+            {
+                _logger.LogInformation($"{pair.Key}:{pair.Value}");
+            }
+
+            var employee = InMemoryData.Employees
+                .SingleOrDefault(x => x.No == request.No);
+            if (employee != null)
+            {
+                var response = new EmployeeResponse
+                {
+                    Employee = employee
+                };
+                return Task.FromResult(response);
+            }
+                throw new Exception($"Employee not found with no : {request.No}");
+        }
+    }
+}
