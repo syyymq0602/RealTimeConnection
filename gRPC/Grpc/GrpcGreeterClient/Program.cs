aea5311fd7d7f6f3de8c9b86bb9e5d127ae8ad51
tpl <tpl_vis_group@outlook.com>
2020-11-20 20:22:38 +0800

    Grpc

---
+++ gRPC/Grpc/GrpcGreeterClient/Program.cs
@@ -0,0 +1,29 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcGreeterClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // The port number(5001) must match the port of the gRPC server.
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+            var re = SayHello(client);
+            
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+        public static async Task SayHello(Greeter.GreeterClient client)
+        {
+            var reply = await client.SayHelloAsync(new HelloRequest
+            {
+                Name = "GrpcClient  ",
+            });
+            Console.WriteLine("Greeting: " + reply.Message);
+        }
+    }
+}
